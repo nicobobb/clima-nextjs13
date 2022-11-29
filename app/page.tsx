fb780f5bd21bd9599da166aacd314c52a321b126
Nico Bobb <nicobobb@gmail.com>
2022-11-29 11:37:40 -0300

    Add TailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,36 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+    <div>
+      <main>
+        <h1 className="text-3xl font-bold underline">
           Welcome to <a href="https://nextjs.org">Next.js 13!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p>
           Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
+          <code>app/page.tsx</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
+        <div>
+          <a href="https://beta.nextjs.org/docs">
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js 13</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            href="https://github.com/vercel/next.js/tree/canary/examples"           
           >
             <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
+            <p>Explore the Next.js 13 playground.</p>            
           </a>
 
           <a
             href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            className={styles.card}
+          
           >
             <h2>Deploy &rarr;</h2>
             <p>Deploy your Next.js site to a public URL with Vercel.</p>
@@ -40,14 +38,14 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
